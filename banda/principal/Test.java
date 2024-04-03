3c81c374e610aa1bbd3d78f2625edeb006b9d572
juanx199 <132611235+juanx199@users.noreply.github.com>
2024-04-03 07:52:43 -0500

    Add files via upload

---
+++ banda/principal/Test.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.com.juanx199.banda.principal;
+
+import co.com.juanx199.banda.logica.Banda;
+import co.com.juanx199.banda.logica.*;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Test {
+    public static void main (String[] args){
+        Banda b = new Banda();
+        b.conformarBanda(5);
+        b.afinarBanda();
+        b.tocarrBanda();
+    }
+}
